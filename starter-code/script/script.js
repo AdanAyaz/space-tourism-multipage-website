09064c2203b6d8be65e36d952daea1c83c208d2a
AdanAyaz <91949524+AdanAyaz@users.noreply.github.com>
2021-12-06 19:09:00 +0500

    Add files via upload

---
+++ starter-code/script/script.js
@@ -0,0 +1,14 @@
+// NAVBAR FUNCTIONALITY
+
+let desktopTab0 = document.getElementById("navigation__list--item-0");
+let desktopTab1 = document.getElementById("navigation__list--item-1");
+let desktopTab2 = document.getElementById("navigation__list--item-2");
+let desktopTab3 = document.getElementById("navigation__list--item-3");
+
+desktopTab0.style.borderBottom = "solid 3px white";
+// for (i = 0; i > 3; i++) {
+//   desktopTab[i].addEventListener("click", function () {
+//     desktopTab[i].style.borderBottom = "solid 3px white";
+//   });
+// }
+console.log(currentTab);
